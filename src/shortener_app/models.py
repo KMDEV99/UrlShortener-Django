45eabad11807fecf68e4ec4b8a870b0d5e3e2a1f
KMDEV99 <matkonrad99@gmail.com>
2020-11-14 20:25:49 +0100

    created url_generator

--- src/shortener_app/models.py
+++ src/shortener_app/models.py
@@ -1,11 +1,20 @@
 from django.db import models
-
+from string import ascii_lowercase, digits
+from random import choice
 # Create your models here.
+
+def url_generator(size=6, chars=ascii_lowercase + digits):
+	return ''.join(choice(chars) for _ in range(size))
 
 class ShortenerURL(models.Model):
 	url = models.CharField(max_length=200, )
-	shortcode = models.CharField(max_length=20, unique=True)
-	timestamp = models.DateTimeField(auto_now_add=True)
+	short_url = models.CharField(max_length=20, unique=True)
+	create_date = models.DateTimeField(auto_now_add=True)
+
+	def save(self, *args, **kwargs):
+		self.short_url = url_generator()
+		super(ShortenerURL, self).save(*args, **kwargs)
+
 	def __str__(self):
 		return str(self.url)
 
