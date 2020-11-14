55df55e28961f7e6bad2bd9f249fa127d5732f65
KMDEV99 <matkonrad99@gmail.com>
2020-11-14 22:14:25 +0100

    added print_urls admin command

--- src/shortener_app/models.py
+++ src/shortener_app/models.py
@@ -6,14 +6,22 @@ from .utils import url_generator, create_short_url
 
 class ShortenerURLManager(models.Manager):
 	def all(self, args, **kwargs):
-		qs = super(ShortenerURLManager, self).all(*args, **kwargs)
+		qs_main = super(ShortenerURLManager, self).all(*args, **kwargs)
+		qs = qs_main.filter(active=True)
 		return qs
 
+	def print_urls(self, print_all=False):
+		qs = ShortenerURL.objects.filter(id__gte=1) if print_all else ShortenerURL.objects.filter(id__gte=1)[:5]
+		urls = [(url.url, url.short_url) for url in qs]
+		return str(urls)
+
 class ShortenerURL(models.Model):
-	url = models.CharField(max_length=200, )
+	url = models.CharField(max_length=200)
 	short_url = models.CharField(max_length=20, unique=True, blank=True)
 	create_date = models.DateTimeField(auto_now_add=True)
 	active = models.BooleanField(default=True)
+
+	objects = ShortenerURLManager()
 
 	def save(self, *args, **kwargs):
 		if not self.short_url:
