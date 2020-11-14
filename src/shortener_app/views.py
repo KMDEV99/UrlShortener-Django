f949f6e8807accb56800e41ca81a37eadb34c413
KMDEV99 <matkonrad99@gmail.com>
2020-11-14 23:17:36 +0100

    cleaning up views and urls

--- src/shortener_app/views.py
+++ src/shortener_app/views.py
@@ -1,11 +1,21 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
 
+from .models import ShortenerURL
 # Create your views here.
-def shortener_redirect_view(request, *args, **kwargs):
-	return HttpResponse("hello")
+
+
+class HomeView(View):
+	def get(self, request, *args, **kwargs):
+		return render(request, "home.html", {})
+
 
 class ShortenerCBView(View):
-	def get(self, request, *args, **kwargs):
-		return HttpResponse("Hello again")
+	def get(self, request, short_url=None, *args, **kwargs):
+		obj_url = None
+		qs = ShortenerURL.objects.filter(short_url__iexact=short_url.upper())
+		if qs.exists and qs.count() == 1:
+			obj = qs.first()
+			obj_url = obj.url
+		return HttpResponse("hello {short_url}".format(short_url=obj_url))
