36d2a4c27933e2e78601ab0a625bffdd9c9b5f57
KMDEV99 <matkonrad99@gmail.com>
2020-11-15 00:06:30 +0100

    added home view fbv

--- src/shortener_app/views.py
+++ src/shortener_app/views.py
@@ -6,10 +6,19 @@ from .models import ShortenerURL
 # Create your views here.
 
 
+def home_view_fbv(reques, *args, **kwargs):
+	if request.method == 'POST':
+		print(request.POST)
+	return render(request, "shortener/home.html", {})
+
 class HomeView(View):
 	def get(self, request, *args, **kwargs):
 		return render(request, "shortener/home.html", {})
 
+	def post(self, request, *args, **kwargs):
+		print(request.POST)
+		print(request.POST.get('url'))
+		return render(request, "shortener/home.html", {})
 
 class ShortenerCBView(View):
 	def get(self, request, short_url=None, *args, **kwargs):
